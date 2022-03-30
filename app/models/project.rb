87bafaf8a05493a9e32aca52ec1fe992a25720bc
rupesh <rupesh.shrestha96742@gmail.com>
2022-03-30 15:59:52 +0545

    Add default column values

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,13 @@
 class Project < ApplicationRecord
-  # Custom scopes
-  # Add defaults
-  # Integrate validations
-  # Integrate callbacks
-  # Integrating database relationships
+  # Add default column values
+  after_initialize :set_defaults
+
+  # Custom scoping
   scope :almost_completed, -> { where('percent_complete > 75.0') }
   scope :still_needs_some_work, -> { where('percent_complete < 75.0') }
+
+  def set_defaults
+    self.percent_complete ||= 0.0 # if value is nil set to 0 else don't do anything
+  end
+
 end
